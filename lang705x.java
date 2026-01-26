4bcb69b9fb1c5b7b254ec7d277b232e297f966ad
Lambeau <155316702+lambeau7@users.noreply.github.com>
2026-01-26 10:17:55 -0600

    Update main method to handle file input
    Refactor main method to read integers and strings from files.

--- lang705x.java
+++ lang705x.java
@@ -2,12 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class lang705x {
-    public static void main(String[] args) {
-       
-
-
-
-
+    public static void main(String[] args) throws IOException 
+    {
+        ArrayList<Int> num = new ArrayList<int>();
+        ArrayList<String[]> poem = new ArrayList<String[]>();
+        Scanner r = new Scanner(new File("prog705x.txt"));
+        while(r.hasNextInt())
+        {
+            num.add(r.nextInt());
+        }
+        Scanner er = new Scanner(new File("prog512h.txt"));
+        while(er.hasNext())
+        {
+            String temp = er.nextLine();
+            String[] temps = temp.split(" ");
+            poem.add(temps);
+        }
+        
 
     }
 }
